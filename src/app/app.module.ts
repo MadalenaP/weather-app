beeb19898de564047fa1b616c0e2291a86f05d30
Maddy <magdalini.palaiologou@siemens.com>
2024-02-17 10:14:52 +0100

    loading indicator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TodayComponent } from './today/today.component';
 import { NextDaysComponent } from './next-days/next-days.component';
+import { LoadingIndicatorComponent } from './loading-indicator/loading-indicator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodayComponent,
-    NextDaysComponent
+    NextDaysComponent,
+    LoadingIndicatorComponent
   ],
   imports: [
     BrowserModule,
